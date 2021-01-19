9358e4939f157c431906e40e43c90ef542b99b4a
李冬青 <lidongqing@lidongqingdeMacBook-Pro.local>
2021-01-19 13:12:07 +0800

    update

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    title: 'DQblog',
+    description: 'DQblog的网站，专注于前端技术栈分享~',
+    themeConfig: {
+        nav: require('./nav'),
+        sidebar:require('./sidebar')
+    }
+}
+
+
